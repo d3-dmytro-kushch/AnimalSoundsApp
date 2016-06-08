a226a4823ba3938e8b68a78fdcc3ec15419eda3d
Dima <dimak@designcontest.com>
2016-06-08 16:08:11 +0300

    test deviceready event

--- js/service/InitService.js
+++ js/service/InitService.js
@@ -32,7 +32,7 @@ MyApp.angular.factory('InitService', ['$document', function ($document) {
       if (document.URL.indexOf("http://") === -1 && document.URL.indexOf("https://") === -1) {
         // Cordova
         alert("Using Cordova/PhoneGap setting");
-        alert(JSON.stringify(device))
+        alert(device.model);
         document.addEventListener("deviceready", onReady, false);
       } else {
         // Web browser
