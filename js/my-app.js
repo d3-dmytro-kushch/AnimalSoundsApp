9b467c90423adc6df62ad7e7330d7a6f7cf9b204
Dima <dimak@designcontest.com>
2016-06-09 13:16:23 +0300

    audio test

--- js/my-app.js
+++ js/my-app.js
@@ -13,6 +13,32 @@ var view2 = myApp.addView('#view-2', {
 var view3 = myApp.addView('#view-3');
 var view4 = myApp.addView('#view-4');
 
-document.addEventListener("backbutton", function(e){
-    alert('backbutton clicked');
-}, false);
+if( window.plugins && window.plugins.NativeAudio ) {
+
+    // Preload audio resources
+    window.plugins.NativeAudio.preloadComplex( 'music', 'audio/music.mp3', 1, 1, 0, function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+
+    window.plugins.NativeAudio.preloadSimple( 'click', 'audio/click.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+
+
+    // Play
+    window.plugins.NativeAudio.play( 'click' );
+    window.plugins.NativeAudio.loop( 'music' );
+
+
+    // Stop multichannel clip after 60 seconds
+    window.setTimeout( function(){
+
+        window.plugins.NativeAudio.stop( 'music' );
+
+        window.plugins.NativeAudio.unload( 'music' );
+        window.plugins.NativeAudio.unload( 'click' );
+
+    }, 1000 * 60 );
+}
